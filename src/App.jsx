40f8239d401274831d95c45063e58ee5253b7520
apu <apuhot168@gmail.com>
2022-07-01 02:09:50 +0600

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-
-import './App.css';
+import Header from "./component/Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    <h4 className='text-green-400 bg-slate-600 w-11/12 m-auto '> Tailwind css</h4>
-    
-      
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
 export default App;
-
-
---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+
+    <h4 className='text-green-400 bg-slate-600 w-11/12 m-auto '> Tailwind css</h4>
+    
+    
+    </>
+  )
+}
+
+export default Header
