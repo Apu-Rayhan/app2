fee9377201d49078f2338c6deb6d11b1f8b517dd
apu <apuhot168@gmail.com>
2022-07-04 04:12:06 +0600

    tt

--- src/BeciComponent/Footer.jsx
+++ src/BeciComponent/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   AiFillFacebook,
   AiFillLinkedin,
@@ -10,15 +11,16 @@ import { BsTelegram } from "react-icons/bs";
 const Footer = () => {
   return (
     <>
-      <div className=" flex justify-center items-center h-40 bg-indigo-900 text-white ">
-        <div>
-          <AiFillFacebook className=" mr-4" />
-          <AiFillLinkedin className=" mr-4" />
-          <AiOutlineWhatsApp className=" mr-4" />
-          <BsTelegram className=" mr-4" />
+      <div className=" flex flex-col justify-center items-center h-40 bg-indigo-900 text-white ">
+        <div className=" flex w-96 h-16 justify-center items-center    ">
+          <Link to="/#">
+            <AiFillFacebook className=" mr-4 text-2xl " />
+          </Link>
+          <AiFillLinkedin className=" mr-4 text-2xl " />
+          <AiOutlineWhatsApp className=" mr-4 text-2xl " />
+          <BsTelegram className=" mr-4 text-2xl " />
           <AiOutlineMail className=" mr-4 text-2xl   " />
         </div>
-
         <h2>The copyright symbol: &copy;</h2>
       </div>
     </>
