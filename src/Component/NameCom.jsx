9220f9c3cbef6e04be9dfd6c76d983126f1c994e
apu <apuhot168@gmail.com>
2022-07-08 03:37:36 +0600

    ss

--- src/Component/NameCom.jsx
+++ src/Component/NameCom.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Socialicon from "./Social-icon";
 
-const NameCom = () => {
+const NameCom = ({ props}) => {
   return (
     <>
-      <div className=" w-4/5 h-96 border m-auto  ">
-        <h1>Hello </h1>
+      <div className=" w-4/5 h-96 border m-auto flex flex-col items-center ">
+        <div className=" border w-11/12 h-1/2 m-auto flex flex-col text-center justify-center items-center  ">
+          <h1 className=" text-2xl text-red-700 ">{props.work}</h1>
+          <p>{props.para}</p>
+        </div>
+        <Socialicon />
       </div>
     </>
   );
