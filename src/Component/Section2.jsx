0e6f372aba252c7030fbc83c004ea44890e00800
apu <apuhot168@gmail.com>
2022-07-27 02:38:31 +0600

    jul27

--- src/Component/Section2.jsx
+++ src/Component/Section2.jsx
@@ -4,10 +4,11 @@ const Section2 = () => {
   return (
     <>
       <section className=" border w-full hight mt-2    ">
-        <div className=" border  w-full h-20 justify-center items-center flex">
-          <h1 className="">Our Service </h1>
+        <div className=" border bg-slate-600 mt-4  w-full h-20 justify-center items-center flex">
+          <div className=' bg-black h-1 w-96'></div><h1 className=" text-3xl font-bold">Our Service </h1><div className=' bg-black h-1 w-96'></div>
+          
         </div>
-        <div className=" flex space-x-4 justify-around items-center  ">
+        <div className=" flex space-x-4 justify-around items-center mt-4  ">
           <ServiceCard
             props={{
               serviceName: "Web Developer ",
@@ -41,7 +42,7 @@ const ServiceCard = ({ props }) => {
   return (
     <>
       <div className=" border w-1/4 h-80  rounded-md  ">
-        <h1 className=" w-full border h-20 mt-5  font-black text-center text-2xl text-red-600  ">
+        <h1 className=" w-full border h-14 mt-5  font-bold text-center text-2xl text-red-600  ">
           {props.serviceName}
         </h1>
         <p className=" text-center border mt-2  ">{props.serviceDeteles}</p>
--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -3,14 +3,19 @@ import React from "react";
 const Skills = () => {
   return (
     <>
-      <section className=" border w-full hight ">
-        <div className="border w-3/4 h-96 m-auto ">
-          <div className="border ">
-            <h1> professional Skills </h1>
+      <section className=" border w-full hight mt-5 ">
+        <div className="border w-3/4 h-96 m-auto mt-5 ">
+          <div className="border h-16 flex justify-center items-center font-bold text-3xl uppercase ">
+            <h1 className="  "> professional Skills </h1>
           </div>
 
-          <div className="border  m-4 ">
-            <ProgressBar per="90" witd="100" />
+          <div className="border m-auto mt-5 w-2/4">
+            <ProgressBar witd= 'w-11/12' color='bg-lime-500' html='Html'/>
+            <ProgressBar witd= 'w-11/12' color='bg-red-500' html="CSS" />
+            <ProgressBar witd= 'w-2/3' color='bg-red-300' html="Javascript" />
+            <ProgressBar witd= 'w-2/3' color='bg-red-200' html="React js " />
+            <ProgressBar witd= 'w-2/3' color='bg-red-100' html="Node js " />
+            
           </div>
         </div>
       </section>
@@ -21,16 +26,10 @@ const Skills = () => {
 const ProgressBar = (props) => {
   return (
     <>
-      <progress
-        className="block m-1 w-1/2 h-6  "
-        max={props.witd}
-        value={props.per}
-      >
-        50%
-      </progress>
-
-      <div className=" w-2/3 h-6 rounded-md bg-slate-500">
-        <div className=" w-2/3 h-6 bg-lime-500 rounded-md "> </div>
+      
+      <div className=" w-full h-7 rounded-xl bg-slate-500 my-2">
+        <div className={` h-7 rounded-xl ${props.color} ${props.witd} text-black` }> {props.html} </div>
+        
       </div>
     </>
   );
