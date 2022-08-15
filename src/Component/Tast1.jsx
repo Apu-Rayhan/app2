7379c7e46f3e899d0f86fae114ae04c0d6c4d072
apu <apuhot168@gmail.com>
2022-08-16 02:00:39 +0600

    Aug 16

---
+++ src/Component/Tast1.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Tast1 = () => {
+  return (
+    <section className="continer">
+      <div className=" mine">
+        <div className="left ">
+        <img className=" w-80 h-80 " src="https://www.seekpng.com/png/detail/57-576498_man-face-png-happy-people-face-png.png" alt="just"/>
+        </div>
+        <div className="right">
+        <div className="clas1">
+        <h1>DAVID <span>PARKER</span></h1>
+        <p>Creativ Freelancer</p>
+        </div>
+        <div className="clas2">
+        <p>My name is David Parker. I am a graphic designer, and I'm very passionate and dedicated to my work. With 20 years experience as a professional a graphic designer, I have acquired the skills and knowledge.</p>
+        <button type="button">Click Me!</button>
+        </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tast1;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Imgbb from "../Component/ImgBB";
+import Tast1 from "../Component/Tast1";
 
 const Project = () => {
   return (
     <>
       <Imgbb />
+      <Tast1/>
     </>
   );
 };
