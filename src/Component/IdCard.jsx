71300822b127c509e07fe744e40cf2aabca3c8b5
opu-rayhan <apuhot168@gmail.com>
2022-09-19 04:22:08 +0600

    sep 19

--- src/Component/IdCard.jsx
+++ src/Component/IdCard.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const IdCard = () => {
+  const [users, setUsers] = useState([]);
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+  // The separate async function
+  const getUsers = async () => {
+    const response = await fetch(API_URL);
+    const userdata = await response.json();
+    setUsers(userdata);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  console.log(users[0]);
+
   return (
     <>
       <section className="continer">
-        <div className=" w-1/4 m-auto my-10 h-60 border   ">
-        
-        <h1 className="  "> Compani Name </h1>
-        <img src="https://www.ansarvdpcollege.edu.bd/images/student_img/190050039.png" alt="png" srcset="" className=" w-16 h-16 mt-2 m-auto rounded-full " />
-        <hr />
-
-        <h5>Name :</h5>
-
-      
-        
-
-        
-        
+        <div className=" flex flex-wrap m-auto">
+          {users.map((user) => (
+            <div key={user.id} className=" w-1/4 my-10 mx-10 border   ">
+              <h5> ID Non : {user.id}</h5>
+              <h5> Name : {user.name}</h5>
+              <h5> UserName : {user.username}</h5>
+              <h5> Email : {user.email}</h5>
+              <h5> WebSite : {user.website}</h5>
+              <h5> Phone : {user.phone}</h5>
+              <h5> Company Name : {user.company.name}</h5>
+              <h5> City Name : {user.address.city}</h5>
+              <h5> ZipCode : {user.address.zipcode}</h5>
+            </div>
+          ))}
         </div>
       </section>
     </>
