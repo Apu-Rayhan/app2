f1e859c3c421a8edd3491f82f5391cef069696d2
opu-rayhan <apuhot168@gmail.com>
2022-09-19 02:42:48 +0600

    sep 19

--- src/Component/IdCard.jsx
+++ src/Component/IdCard.jsx
@@ -7,10 +7,12 @@ const IdCard = () => {
         <div className=" w-1/4 m-auto my-10 h-60 border   ">
         
         <h1 className="  "> Compani Name </h1>
-        <img src="https://pbs.twimg.com/media/FFq_7ccVQAAmI9-.png" alt="png" srcset="" className=" w-16 h-16 mt-2 m-auto rounded-full " />
+        <img src="https://www.ansarvdpcollege.edu.bd/images/student_img/190050039.png" alt="png" srcset="" className=" w-16 h-16 mt-2 m-auto rounded-full " />
         <hr />
 
         <h5>Name :</h5>
+
+      
         
 
         
