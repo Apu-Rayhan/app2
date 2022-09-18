7fe75d2123207680f4bfeabeb3d8ab5e41616320
opu-rayhan <apuhot168@gmail.com>
2022-09-19 02:42:24 +0600

    sep 19

---
+++ src/Component/IdCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const IdCard = () => {
+  return (
+    <>
+      <section className="continer">
+        <div className=" w-1/4 m-auto my-10 h-60 border   ">
+        
+        <h1 className="  "> Compani Name </h1>
+        <img src="https://pbs.twimg.com/media/FFq_7ccVQAAmI9-.png" alt="png" srcset="" className=" w-16 h-16 mt-2 m-auto rounded-full " />
+        <hr />
+
+        <h5>Name :</h5>
+        
+
+        
+        
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default IdCard;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import IdCard from "../Component/IdCard";
 import Imgbb from "../Component/ImgBB";
 import Tast1 from "../Component/Tast1";
+
 
 const Project = () => {
   return (
     <>
       <Imgbb />
       <Tast1/>
+      <IdCard/>
     </>
   );
 };
