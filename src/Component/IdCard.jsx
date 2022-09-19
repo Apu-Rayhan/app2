3bc1b456ed4731aecc6354465367e1bffd8e184b
opu-rayhan <apuhot168@gmail.com>
2022-09-20 03:07:20 +0600

    sep 20

--- src/Component/IdCard.jsx
+++ src/Component/IdCard.jsx
@@ -19,19 +19,22 @@ const IdCard = () => {
 
   return (
     <>
-      <section className="continer">
-        <div className=" flex flex-wrap m-auto">
+      <section className="continer  ">
+        <div className=" flex flex-wrap  ">
           {users.map((user) => (
-            <div key={user.id} className=" w-1/4 my-10 mx-10 border   ">
+            <div key={user.id} className=" w-1/4 my-5 mx-5 rounded-2xl  p-10 bg-slate-800  ">
+
+            <h5 className=" flex justify-center items-center pl-10"> Company Name <br/>{user.company.name}</h5>
+            <img src="https://www.ansarvdpcollege.edu.bd/images/student_img/190050039.png" alt="sempol" srcset="" className=" w-24 inline my-5 ml-16" />
+
               <h5> ID Non : {user.id}</h5>
               <h5> Name : {user.name}</h5>
               <h5> UserName : {user.username}</h5>
               <h5> Email : {user.email}</h5>
               <h5> WebSite : {user.website}</h5>
               <h5> Phone : {user.phone}</h5>
-              <h5> Company Name : {user.company.name}</h5>
-              <h5> City Name : {user.address.city}</h5>
-              <h5> ZipCode : {user.address.zipcode}</h5>
+              <p> City Name : {user.address.city} <span>ZipCode : {user.address.zipcode}</span></p>
+              
             </div>
           ))}
         </div>
